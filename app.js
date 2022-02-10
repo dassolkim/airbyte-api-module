3615229b771a697a20e06ee68713a5f783c6d278
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-10 20:21:25 +0900

    add discover_schma and delete

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-// const { json } = require('express/lib/response');
 const uuidv1 = require('uuid/v1')
-// const https = require('https')
-// const parsejson = require('parse-json')
 const rp = require('request-promise-native')
-const JSON = require('fast-json-stable-stringify')
 const request = require('request');
 const res = require('express/lib/response');
 
-var jsonDataObj = {destinationId: "3ca90345-492e-44f8-95cd-72cb2616af33"};
 const datasetId = uuidv1()
 const workspaceId = "ff385b87-9469-47cb-9733-4eb23d771987"
 const destinationDefinitionId = "25c5221d-dce2-4163-ade9-739ef790f503"
 const defaultUrl = "http://114.70.235.40:18000/api/v1/"
 const sourceDefinitionId = "decd338e-5647-4c0b-adf4-da0e75f5a750"
 
-// const options = {
-//     uri: defaultUrl + "destinations/list",
-//     method: 'POST',
-//     body: {
-//         workspaceId: workspaceId
-//     },
-//     json: true,
-//   };
-//   request.post(options,function(err, response, body){
-//       console.log(response.body);
-//   })
-
-function returnBody(){
-
+function main(){
+    console.log("discover logic is start")
+    console.log("datasetId: ", datasetId)
+    discoverLogic()
+}
+if (require.main == module){
+    main()
 }
 
 function createSource() {
-    
-
     var url = defaultUrl + "sources/create"
     var connectionConfiguration = {
         "host": "114.70.235.40",
@@ -52,13 +38,15 @@ function createSource() {
             connectionConfiguration: connectionConfiguration,
             name: sourceName
         },
-      };
-    return rp(options, function (respose, body) {
-        var res = JSON(body);
-        return res;
+    };
+    return rp(options, function (error, respose, body) {
+        if (error) {
+            console.log('error');
+        } else {
+            var result = body;
+            return result;
+        }
     })
-    
-
     // request.post(options, function (err, response, body){
     //       console.log("create source complete")
     //       const res = body
@@ -77,40 +65,86 @@ function getSource(sourceId){
         body: {
             sourceId: sourceId
         },
-        json: true,
-      };
-      request.post(options,function(err, response, body){
-          console.log("get source complete")
-          console.log(response.body)
-          requestIdleCallback(body)
-      })
+        json: true
+    };
+    return rp(options, function (error, respose, body) {
+        if (error) {
+            console.log('error');
+        } else {
+            var result = body;
+            return result;
+        }
+    })
 
 }
-function discoverSchema() {
+
+function discoverSchema(sourceId) {
     var url = defaultUrl + "sources/discover_schema"
-    var sourceId = ""
+    var sourceId = sourceId
     const options = {
         uri: url,
         method: 'POST',
         body: {
-            workspaceId: workspaceId,
-            sourceDefinitionId: sourceDefinitionId,
-            connectionConfiguration: connectionConfiguration,
-            name: sourceName
+            sourceId: sourceId
         },
-        json: true,
-      };
-      request.post(options,function(err, response, body){
-          console.log("discover_shcema complete")
-          console.log(response.body);
-      })
+        json: true
+    };
+    return rp(options, function (error, respose, body) {
+        if (error) {
+            console.log('error');
+        } else {
+            var result = body
+            return result;
+        }
+    })
+}
+
+function deleteSource(sourceId) {
+    var url = defaultUrl + "sources/delete"
+    var sourceId = sourceId
+    const options = {
+        uri: url,
+        method: 'POST',
+        body: {
+            sourceId: sourceId
+        },
+        json: true
+    };
+    return rp(options, function (error, respose, body) {
+        if (error) {
+            console.log('error');
+        } else {
+            var result = body;
+            return result;
+        }
+    })
 }
 
 async function discoverLogic() {
-    var sources = await createSource();
-    console.log(sources)
-    var sourceId = sources.sourceId
-    console.log("sourceId: ", sourceId)
-    
+    try{
+        console.time('api call during time')
+        var source = await createSource()
+        // console.log(source)
+        if (source != null){
+            var sourceId = source.sourceId
+            console.log("sourceId: ", sourceId)
+            var getSourceResult = await getSource(sourceId)
+            console.log(getSourceResult)
+            console.log("source lookup is done")
+        } else { console.log("get source api does not work")}
+        if (getSourceResult != null){
+            var discoverResult = await discoverSchema(sourceId)
+            console.log(JSON.stringify(discoverResult, null, 2))
+            console.log("source validation is done")
+        } else { console.log("discover_schema does not work")}
+        if (discoverResult != null){
+            var deleteSourceResult = await deleteSource(sourceId)
+            console.log(deleteSourceResult)
+            console.log("source deletion is done")
+        } else { console.log("delete source api does not work")}
+        console.timeEnd('api call during time')
+    } catch (error) {
+        console.log(error)
+    }
 }
-discoverLogic();
+

473d95074b0c4c7d632270b0952b2e6025a4fb37
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-10 20:34:46 +0900

    hide info

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const workspaceId = "ff385b87-9469-47cb-9733-4eb23d771987"
 const destinationDefinitionId = "25c5221d-dce2-4163-ade9-739ef790f503"
 const defaultUrl = "http://114.70.235.40:18000/api/v1/"
 const sourceDefinitionId = "decd338e-5647-4c0b-adf4-da0e75f5a750"
-
+const dbConfig = require('./connectConfig.js')
 function main(){
     console.log("discover logic is start")
     console.log("datasetId: ", datasetId)
@@ -20,13 +20,7 @@ if (require.main == module){
 
 function createSource() {
     var url = defaultUrl + "sources/create"
-    var connectionConfiguration = {
-        "host": "114.70.235.40",
-        "port": 15432,
-        "database": "dbt_custom",
-        "password": "dke!!214!!",
-        "username": "knu"
-    }
+    var connectionConfiguration = dbConfig.connectConf
     var sourceName = "nodeCreateSource"
     const options = {
         uri: url,

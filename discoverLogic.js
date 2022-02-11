c0a79477d1e36ad3543b79fe5690fedc500b862e
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-11 11:42:27 +0900

    FIX: divide configInfo

--- discoverLogic.js
+++ discoverLogic.js
@@ -4,14 +4,10 @@ const request = require('request');
 const res = require('express/lib/response');
 
 const datasetId = uuidv1()
-const workspaceId = "ff385b87-9469-47cb-9733-4eb23d771987"
-const destinationDefinitionId = "25c5221d-dce2-4163-ade9-739ef790f503"
-const defaultUrl = "http://114.70.235.40:18000/api/v1/"
-const sourceDefinitionId = "decd338e-5647-4c0b-adf4-da0e75f5a750"
-const dbConfig = require('./connectConfig.js')
+const configInfo = require('./connectConfig.js')
 
 function main(){
-    console.log("discover logic is start")
+    console.log("Start discoverLogic")
     console.log("datasetId: ", datasetId)
     discoverLogic()
 }
@@ -20,16 +16,16 @@ if (require.main == module){
 }
 
 function createSource() {
-    var url = defaultUrl + "sources/create"
-    var connectionConfiguration = dbConfig.connectConf
+    var url = configInfo.defaultUrl + "sources/create"
+    var connectionConfiguration = configInfo.connectSource
     var sourceName = "nodeCreateSource"
     const options = {
         uri: url,
         method: 'POST',
         json: true,
         body: {
-            workspaceId: workspaceId,
-            sourceDefinitionId: sourceDefinitionId,
+            workspaceId: configInfo.workspaceId,
+            sourceDefinitionId: configInfo.sourceDefinitionId,
             connectionConfiguration: connectionConfiguration,
             name: sourceName
         },
@@ -45,7 +41,7 @@ function createSource() {
 }
 
 function getSource(sourceId){
-    var url = defaultUrl + "sources/get"
+    var url = configInfo.defaultUrl + "sources/get"
     const options = {
         uri: url,
         method: 'POST',
@@ -65,7 +61,7 @@ function getSource(sourceId){
 }
 
 function discoverSchema(sourceId) {
-    var url = defaultUrl + "sources/discover_schema"
+    var url = configInfo.defaultUrl + "sources/discover_schema"
     var sourceId = sourceId
     const options = {
         uri: url,
@@ -86,7 +82,7 @@ function discoverSchema(sourceId) {
 }
 
 function deleteSource(sourceId) {
-    var url = defaultUrl + "sources/delete"
+    var url = configInfo.defaultUrl + "sources/delete"
     var sourceId = sourceId
     const options = {
         uri: url,
@@ -120,6 +116,7 @@ async function discoverLogic() {
         } else { console.log("get source api does not work")}
         if (getSourceResult != null){
             var discoverResult = await discoverSchema(sourceId)
+            var catalog = discoverResult.catalog
             console.log(JSON.stringify(discoverResult, null, 2))
             console.log("source validation is done")
         } else { console.log("discover_schema does not work")}
@@ -132,5 +129,6 @@ async function discoverLogic() {
     } catch (error) {
         console.log(error)
     }
+    console.log("extract catalog: ", JSON.stringify(catalog, null, 2))
 }
 

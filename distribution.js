98f28d5d749af0d9b2d477a06fd7f77a4cd979c6
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-16 19:58:41 +0900

    FIX: main

--- distribution.js
+++ distribution.js
@@ -8,17 +8,17 @@ console.log("datasetId: ", datasetId)
 
 async function main(){
     
-    var delSource = true
-    var delDestination = true
+    var delSource = false
+    var delDestination = false
 
     console.log("Start sourceLogic")
     var discoverLogicReturn = await sourceLogic.discoverLogic(delSource)
     console.log("sourceLogic return: ", discoverLogicReturn)
-    
+
     // right delSource is false
     if (discoverLogicReturn != null && delSource == false){
         console.log("Start destinationLogic")
-        var destinationLogicReturn = await destinationLogic(delDestination)
+        var destinationLogicReturn = await destinationLogic.destinationLogic(delDestination)
         console.log("destinationLogic return: ", destinationLogicReturn)
     }
     

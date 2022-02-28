95181b85fb4d4ecc5a0d0798574f75773899368b
dassolkim <kimds0926@kangwon.ac.kr>
2022-02-28 16:04:20 +0900

    FIX: divide createDistribution

---
+++ createDistribution.js
@@ -0,0 +1,40 @@
+const sourceLogic = require('./airbyte/source/sourceLogic')
+const destinationLogic = require('./airbyte/destination/destinationLogic')
+const connectionLogic = require('./airbyte/connection/connectionLogic')
+const configInfo = require('./config/connectConfig')
+const uuidv1 = require('uuid/v1')
+
+// get datasetId from SODAS+
+
+// const datasetId = uuidv1()
+// console.log("datasetId: ", datasetId)
+// const testCatalog = {}
+async function main(){
+    
+    var delDestination = false
+    
+    console.log("Start sourceLogic")
+    var createLogicReturn = await sourceLogic.createLogic()
+    console.log("sourceLogic return: ", createLogicReturn)
+    
+    console.log("Start destinationLogic")
+    var destinationLogicReturn = await destinationLogic.destinationLogic(delDestination)
+    console.log("destinationLogic return: ", destinationLogicReturn)
+    
+    var data = createLogicReturn
+    data.destinationId = destinationLogicReturn
+    data.status = configInfo.status
+    data.operationIds = [configInfo.operationId]
+    var sync = true
+    // console.log("This is connectionCreate input data: ", data)
+    // console.log(data)
+    console.log("start connectionLogic")
+    var connectionLogicReturn = await connectionLogic.connectionLogic(data, sync)
+    console.log("Connection Logic Return:  ", connectionLogicReturn)
+    
+    
+}
+if (require.main == module){
+    main()
+    // console.log(testCatalog)
+}

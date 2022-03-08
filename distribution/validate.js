3767f53d228bcdb69e897ae9bd85bb5184fdc442
dassolkim <kimds0926@kangwon.ac.kr>
2022-03-08 14:29:36 +0900

    FIX: divide source and distribution validate

--- distribution/validate.js
+++ distribution/validate.js
@@ -1,28 +1,35 @@
 const sourceLogic = require('../airbyte/source/sourceLogic')
 const destinationLogic = require('../airbyte/destination/destinationLogic')
-const uuidv1 = require('uuid/v1')
+// const configInfo = require('../config/connectConfig')
+// const uuidv1 = require('uuid/v1')
 
-// get datasetId from SODAS+
-const datasetId = uuidv1()
-console.log("datasetId: ", datasetId)
+module.exports = {validateSource, validateDestination}
 
-async function main(){
+async function validateSource(sourceInfo){
     
     var delSource = true
-    var delDestination = true
-    
-    console.log("Start sourceLogic")
-    var discoverLogicReturn = await sourceLogic.discoverLogic(delSource)
-    console.log("sourceLogic return: ", discoverLogicReturn)
-     
-    // right delSource is false
-    if (discoverLogicReturn != null && delSource == false){
-        console.log("Start destinationLogic")
-        var destinationLogicReturn = await destinationLogic.destinationLogic(delDestination)
-        console.log("destinationLogic return: ", destinationLogicReturn)
+
+    console.log("Start validationSource")
+    var sourceValidate = await sourceLogic.validateLogic(sourceInfo, delSource)
+    // console.log("source validateLogic return: ", sourceValidate)
+    if (sourceValidate == true) {
+        console.log("validation suceess")
+        return true
     }
-    console.log("distiribution/validate logic is complete")
+
 }
-if (require.main == module){
-    main()
+
+async function validateDestination(destinationInfo){
+    var delDestination = true
+    console.log("Start validateDestination")
+    var destinationLogicReturn = await destinationLogic.destinationLogic(destinationInfo, delDestination)
+    console.log("destinationLogic return: ", destinationLogicReturn)
+    if (destinationLogicReturn != null){
+        console.log("validate distribution failed")
+        return null
+    }
+    console.log("validate distribution is successful")
+    return true
+
+
 }
